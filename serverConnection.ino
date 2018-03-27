7d07bd8b68bbdcc8041f1dcd24d589f8859244b9
Aaron Kammula <aaron.kammula@gmail.com>
2018-03-26 23:52:07 -0400

    Connecting Arduino to the Internet
    Connecting to IoT's server.

--- serverConnection.ino
+++ serverConnection.ino
@@ -2,7 +2,7 @@
 #include <PubSubClient.h> // https://github.com/knolleary/pubsubclient/releases/tag/v2.3
 #include <ArduinoJson.h> // https://github.com/bblanchon/ArduinoJson/releases/tag/v5.0.7
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h> //Imported as an external library, and overwritten by IBM's server after connection.
 
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
